72d43cd534d206994129bb2b890bb29f7692878d
Lilia-Chechina <lilya-chechina@mail.ru>
2024-12-24 16:08:12 +0300

    LaunchScreen

--- CatGenerator/ViewController.swift
+++ CatGenerator/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var catimageView: UIImageView!
+    @IBOutlet weak var textLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func didTapCatGnerateButton(_ sender: Any) {
+//        textLabel.text = "Hello, world!"
+        dowloadCat()
+        
+    }
+    private func dowloadCat(){
+        guard let url = URL(string: "https://cataas.com/cat") else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url){ [weak self]
+            data, response, error in guard let data = data else {
+                return
+            }
+            DispatchQueue.main.async {
+                [weak self] in self?.catimageView.image = UIImage(data: data)
+            }
+        }
+        task.resume()
+    }
 }
 
